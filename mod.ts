4735e940946e475e4d853a092e7b09e36014a014
RolandWarburton <warburtonroland@gmail.com>
2025-04-15 16:09:06 +1000

    Move to mod.ts

---
+++ mod.ts
@@ -0,0 +1,5 @@
+import { LabelGenerator } from "./src/label.ts";
+import { downloadCanvas } from "./src/util.ts";
+
+export { downloadCanvas };
+export default LabelGenerator;
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { LabelGenerator } from "./label.ts";
-import { downloadCanvas } from "./util.ts";
-
-export { downloadCanvas };
-export default LabelGenerator;
