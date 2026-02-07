b1df1ca91a62b306eb65921bb47e5b7abfadfb03
RolandWarburton <warburtonroland@gmail.com>
2026-02-07 22:59:22 +1100

    Fix example provider

--- src/providers/api.ts
+++ src/providers/api.ts
@@ -1,8 +1,11 @@
+// this is an example built-in function
+// the purpose of this is to return a string
+// to be embedded in the QR code
 export async function fetchQRCodeData(
   baseURL: string,
   value: string,
 ): Promise<string> {
-  const response = await fetch(`${baseURL}/qr/${value}`);
+  const response = await fetch(`${baseURL}/qr/encode/${value}`);
   if (!response.ok) throw new Error("Failed to fetch QR data");
   const data = await response.json();
   if (!data.code) throw new Error("API returned no code");
