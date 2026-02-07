4a656bc873e4a6a50a522884072cc135065c84fc
RolandWarburton <warburtonroland@gmail.com>
2026-02-07 18:24:09 +1100

    Add ILabelConfig to module export

--- mod.ts
+++ mod.ts
@@ -1,9 +1,12 @@
 import { LabelGenerator } from "./src/label.ts";
 import { downloadCanvas } from "./src/util.ts";
 import { fetchQRCodeData, fetchQRCodeData_mock } from "./src/providers/api.ts";
+import type { ILabelConfig } from "./src/types/label.ts";
 
 export default LabelGenerator;
 // utility to download the canvas in browsers
 export { downloadCanvas };
+// export types
+export type { ILabelConfig };
 // export these to inject into the label generator
 export { fetchQRCodeData, fetchQRCodeData_mock };
