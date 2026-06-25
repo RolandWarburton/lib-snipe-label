ce48907fe6d3de9cba7073e37f9bc358b0e2f7e2
RolandWarburton <warburtonroland@gmail.com>
2026-06-25 20:32:10 +1000

    Move serve-examples to scripts folder

--- scripts/serve-examples.ts
+++ scripts/serve-examples.ts
@@ -1,6 +1,10 @@
 import { serveDir } from "file-server";
 
 Deno.serve({ port: 3030 }, (req) => {
+  const url = new URL(req.url);
+  if (url.pathname === "/") {
+    return Response.redirect(new URL("/examples/", req.url), 302);
+  }
   return serveDir(req, {
     fsRoot: ".",
     showDirListing: true,
