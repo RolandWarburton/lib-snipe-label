0257de0f5998450ff69efb62d3100e2fd84e50ee
RolandWarburton <warburtonroland@gmail.com>
2026-06-30 17:01:16 +1000

    Improve label api

--- src/label.ts
+++ src/label.ts
@@ -8,7 +8,7 @@ export class LabelGenerator {
     this.config = config;
   }
 
-  public async makeLabel(qrValue: string): Promise<HTMLCanvasElement | Error> {
+  public async makeLabel(qrValue: string): Promise<HTMLCanvasElement> {
     const {
       width,
       height,
@@ -17,48 +17,44 @@ export class LabelGenerator {
       fetchQRData,
     } = this.config;
 
-    try {
-      // get the QR data to be encoded into the QR
-      const qrData = await fetchQRData(qrValue);
-      // create a new canvas
-      const renderer = new CanvasRenderer(width, height);
-      // calculate the width of the QR code
-      const qrWidth = height;
+    // get the QR data to be encoded into the QR
+    const qrData = await fetchQRData(qrValue);
+    // create a new canvas
+    const renderer = new CanvasRenderer(width, height);
+    // calculate the width of the QR code
+    const qrWidth = height;
 
-      // draw the QR code on the canvas
-      await renderer.drawQR(qrData, qrWidth);
+    // draw the QR code on the canvas
+    await renderer.drawQR(qrData, qrWidth);
 
-      // get the remaining width to draw the text
-      const maxTextWidth = width - qrWidth - margin;
+    // get the remaining width to draw the text
+    const maxTextWidth = width - qrWidth - margin;
 
-      // start drawing text 20vh down from the top
-      let yOffset = height * 0.2;
+    // start drawing text 20vh down from the top
+    let yOffset = height * 0.2;
 
-      // go through each line of text
-      for (const block of text) {
-        const fontSize = block.fontSize || 32;
-        // split it into visual lines to draw
-        const lines = calculateLines(
-          block.text,
-          maxTextWidth,
-          fontSize,
-          (t, s) => renderer.measureWidth(t, s),
-        );
+    // go through each line of text
+    for (const block of text) {
+      const fontSize = block.fontSize || 32;
+      // split it into visual lines to draw
+      const lines = calculateLines(
+        block.text,
+        maxTextWidth,
+        fontSize,
+        (t, s) => renderer.measureWidth(t, s),
+      );
 
-        // protect against writing text outside of the label
-        if (yOffset + (fontSize * lines.length) > height) break;
+      // protect against writing text outside of the label
+      if (yOffset + (fontSize * lines.length) > height) break;
 
-        // draw each line on the label
-        for (const line of lines) {
-          renderer.drawText(line, qrWidth + 15, yOffset, fontSize);
-          yOffset += fontSize;
-        }
-        yOffset += 32;
+      // draw each line on the label
+      for (const line of lines) {
+        renderer.drawText(line, qrWidth + 15, yOffset, fontSize);
+        yOffset += fontSize;
       }
-
-      return renderer.canvas;
-    } catch (e) {
-      return e instanceof Error ? e : new Error("Unknown error");
+      yOffset += 32;
     }
+
+    return renderer.canvas;
   }
 }
